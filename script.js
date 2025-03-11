546ffc1d2894ac875f92eea9083efe8f10c4429c
gabrielbryanfranca <gabriel.bryan.franca@escola.pr.gov.br>
2025-03-11 16:08:30 -0300

    15 38 1

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+cont aumentaFonteBotao=document.getElementById('aumentar-fonte')
+let tamanhaAtualFonte=1;
+aumentaFonteBotao.addEventListener('click',function(){
+tamanhoAtual +=document.body.style.fontSize=`${tamanhaAtualFonte}rem`
+})
+
+
+
+
+})
