280357722dadac701f596c387f8569da96965567
gabrielbryanfranca <gabriel.bryan.franca@escola.pr.gov.br>
2025-03-11 16:26:39 -0300

    final do dia

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let tamanhoAtualFonte=1;
 aumentaFonteBotao.addEventListener('click',function(){
 tamanhoAtualFonte +=document.body.style.fontSize=`${tamanhoAtualFonte}rem`
 })
-
+diminuiFonteBotao.addEventListener('click',function(){
+tamanhoAtualFonte -=0.1;
+document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+})
 
 
 
