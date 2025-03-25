a5d41786cdddea27f658f7084f42f82202b8fcea
gabrielbryanfranca <gabriel.bryan.franca@escola.pr.gov.br>
2025-03-25 14:22:52 -0300

    trem

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded',function(){ 
-const botaoAcessibilidade = document.getElementById ('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade')
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-botaoDeAcessibilidade.addEventListener('click',function(){
-botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-
-})
+    })
 
 const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 const diminuiFonteBotao = document.getElementById('diminuir-fonte');
