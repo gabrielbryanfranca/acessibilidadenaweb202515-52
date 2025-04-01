d3a14695594af15e2e2eb3ada50c337cd3c182ec
gabrielbryanfranca <gabriel.bryan.franca@escola.pr.gov.br>
2025-04-01 14:34:00 -0300

    14:33

--- script.js
+++ script.js
@@ -26,11 +26,13 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
-    alternaContraste.addEventListener('click',function ()  {
-document.body.classList.toggle('alto-contraste')
-    })
-})
 
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
+})
 
 
 
