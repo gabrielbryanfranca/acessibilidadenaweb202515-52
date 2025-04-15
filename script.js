adf2e398f2a356f705859f1d076331612adee855
gabrielbryanfranca <gabriel.bryan.franca@escola.pr.gov.br>
2025-04-15 15:58:29 -0300

    catr5aop

--- script.js
+++ script.js
@@ -38,7 +38,9 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 ScrollReveal().reveal('#inicio', { delay: 500 });
-
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
 
 
 
